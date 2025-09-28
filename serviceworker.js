db6f5056bb940d3c50818f438c98cb9fdbec050b
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-09-28 18:28:02 -0500

    Initial commit

---
+++ serviceworker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'pet-explorer-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/contact.html',
+  '/materialize-v1.0.0/materialize/css/materialize.min.css',
+  '/materialize-v1.0.0/materialize/js/materialize.min.js',
+  '/images/pexels-chevanon-1108099.jpg',
+  '/images/pexels-crisdip-35358-128756.jpg',
+  '/images/pexels-frans-van-heerden-201846-1463295.jpg',
+  'images/pexels-photo4passion-1828874.jpg',
+  '/images/pexels-pixabay-372166.jpg',
+  'images/pexels-vika-glitter-392079-18011803.jpg'
+];
+
+// Install event - cache app assets
+self.addEventListener('install', event => {
+  console.log('Service worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate event - clean old caches
+self.addEventListener('activate', event => {
+  console.log('Service worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event - serve cached assets if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
