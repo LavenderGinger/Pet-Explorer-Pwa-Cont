56397b8658d57713bedc10d841bb36971f0dab0d
Lavender Ginger <a_guerragonzalez@mail.fhsu.edu>
2025-10-27 02:12:33 -0500

    This again

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,24 @@
 const CACHE_NAME = 'pet-explorer-cache-v1';
 const urlsToCache = [
-  'index.html',
-  'about.html',
-  'contact.html',
-  'dog.html',
-  'cat.html',
-  'fish.html',
-  'guineapig.html',
-  'parakeet.html',
-  'rabbit.html',
-  'offline.html',
-  'petquiz.html',
-  'css/materialize-v1.0.0/materialize/css/materialize.min.css',
-  'js/materialize-v1.0.0/materialize/js/materialize.min.js',
-  'images/pexels-chevanon-1108099.jpg',
-  'images/pexels-crisdip-35358-128756.jpg',
-  'images/pexels-frans-van-heerden-201846-1463295.jpg',
-  'images/pexels-photo4passion-1828874.jpg',
-  'images/pexels-pixabay-372166.jpg',
-  'images/pexels-vika-glitter-392079-18011803.jpg'
+  '/index.html',
+  '/about.html',
+  '/contact.html',
+  '/dog.html',
+  '/cat.html',
+  '/fish.html',
+  '/guineapig.html',
+  '/parakeet.html',
+  '/rabbit.html',
+  '/offline.html',
+  '/petquiz.html',
+  '/css/materialize-v1.0.0/materialize/css/materialize.min.css',
+  '/js/materialize-v1.0.0/materialize/js/materialize.min.js',
+  '/images/pexels-chevanon-1108099.jpg',
+  '/images/pexels-crisdip-35358-128756.jpg',
+  '/images/pexels-frans-van-heerden-201846-1463295.jpg',
+  '/images/pexels-photo4passion-1828874.jpg',
+  '/images/pexels-pixabay-372166.jpg',
+  '/images/pexels-vika-glitter-392079-18011803.jpg'
 ];
 
 // Install event - cache app assets
